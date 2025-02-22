1c28ead931eff64b3aff45b7993c5c64b8128a50
Sukhpal0 <ssandhutext@gmail.com>
2025-02-22 11:56:25 -0500

    Ques #2

---
+++ NumbertrianglePattern.java
@@ -0,0 +1,20 @@
+package loops;
+
+public class NumbertrianglePattern{
+    public static void User(int n) {
+        
+        for (int i =1;i <=n;i++){
+           for (int j=1;j<=n-i; j++) {
+                System.out.print(" ");
+            }  
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int n=6;
+        User(n);
+    }
+}
+
